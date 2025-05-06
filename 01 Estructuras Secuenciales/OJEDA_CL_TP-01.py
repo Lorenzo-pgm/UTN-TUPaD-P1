09987e267ea832f10feb8efb525628d7cf8b3e96
L.O <lorenzomkt.0@gmail.com>
2025-05-06 00:24:33 -0300

    Update and rename MI-APELLIDO_TP-01.py to OJEDA_CL_TP-01.py
    TP_01_EST.SECUENCIALES

--- 01 Estructuras Secuenciales/OJEDA_CL_TP-01.py
+++ 01 Estructuras Secuenciales/OJEDA_CL_TP-01.py
@@ -1,13 +1,12 @@
-#Actividad N° 1
+#-1
 print("Hola Mundo! ")
 
-#Actividad N° 2
+#-2
 nombre = input("Ingrese su nombre: ")
 saludo = (f"Hola {nombre}")
 print(saludo)
 
-
-#Actividad N° 3
+#-3
 nombre = input("Ingrese su nombre: ")
 apellido = input("Ingrese su apellido: ")
 edad = input("Ingrese su edad: ")
@@ -15,9 +14,7 @@ lugar = input("Ingrese lugar de residencia: ")
 datos_personales = f"Soy {nombre} {apellido},tengo {edad} años y soy {lugar}. "
 print(datos_personales)
 
-
-#Actividad N° 4
-
+#-4
 radio = 1
 pi = 3.14159
 
@@ -29,9 +26,7 @@ input("Ingrese el valor del radio: ")
 print("El área del circulo es: ", area)
 print("El perimetro del circulo es: ", perimetro)
 
-
-#Actividad N° 5
-
+#-5
 #Solicito al usuario la cantidad de segundos.
 segundos = int(input("Ingese la cantidad de segundos: "))
 
@@ -41,11 +36,10 @@ minutos = int(segundos % 3600) // 60
 
 print(f"{segundos} segundos equivale a {horas} horas y {minutos} minutos. ")
 
-
-#Actividad N° 6
-
+#-6
+#Solicito al usuario Ingrese un numero. 
 numero = int(input("Ingrese un numero y vera su correspondiente tabla: "))
-
+#Se imprime por consola los resultados de la tabla elegida.
 print(f"Tabla de multiplicar del {numero}:")
 print(f"{numero} x 1 = {numero*1}")
 print(f"{numero} x 2 = {numero*2}")
@@ -59,23 +53,23 @@ print(f"{numero} x 9 = {numero*9}")
 print(f"{numero} x 10 = {numero*10}")
 
 
-#Actividad N° 7
-
+#-7
+#Se solicita al usuario ingrese numero entero.
 num = input("Ingrese un numero entero mayor a 0 : ")
 num = int(num)
 print("El numero ingresado es: ", num)
-
+#Se solicita al usuario ingrese otro numero entero.
 num_2 = input("Ingrese otro numero entero mayor a 0 : ")
 num_2 = int(num_2)
 print("El numero ingresado es: ", num_2)
-
+#operaciones matematicas.
 SUMA = print("La suma de ambos es: ", num + num_2)
 RESTA = print("La resta de ambos es: ", num - num_2)
 MULTIPLICACION = print("Su producto ambos es: ", num * num_2)
 DIVISIÓN = print("La division ambos es: ", num // num_2)
 
-#Actividad N° 8
-
+#-8
+#Calculadora de IMC
 mts = input("Ingrese su altura: ")
 mts = float(mts)
 kg = input("Ingrese su peso: ")
@@ -85,9 +79,8 @@ IMC = kg // mts**2
 IMC = int(IMC)
 print(f"Su IMC equivale a: {IMC}")
 
-
-#Actividad N° 9
-
+#-9
+#Se calcula la temperatura de Celsius a Fahrenheit
 celsius = input("Ingrese un valor en Celsius: ")
 celsius = float(celsius)
 
@@ -96,8 +89,8 @@ fahrenheit = (celsius * 9//5 + 32)
 print(f"La temperatura en grados fahrenheit es: {fahrenheit} °F")
 
 
-#Actividad N° 10
-
+#-10
+#Se calcula el promedio de una materia.
 x = float(input("Ingrese un numero: "))
 y = float(input("Ingrese el 2do numero: "))
 z = float(input("Ingrese el 3er numero: "))
